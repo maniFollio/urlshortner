89b092dd7dfff1b301b345bf0cdd8eb6b5aac74f
Manish <www.mehta70@gmail.com>
2026-05-08 01:19:09 +0530

    fix deployed short url

--- connectMongo.js
+++ connectMongo.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
+        console.log("Connect to MongoDB successfully")
         await mongoose.connect(process.env.MONGODB_CONNECT_URI)
         console.log("Connect to MongoDB successfully")
     } catch (error) {
-        console.log("Connect failed " + error.message )
+        console.log("Connect failed " + error.message );
+        throw error;
     }
 }
 
